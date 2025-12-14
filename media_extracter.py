c0557f630fa1bfd0dd57c6fdec5e9f0f1df47907
lemon-gith <77743782+lemon-gith@users.noreply.github.com>
2025-12-14 23:35:27 +0000

    add code to repo

---
+++ media_extracter.py
@@ -0,0 +1,131 @@
+import re
+import json
+import os
+import glob
+import requests
+import io
+import PIL.Image as Image
+
+
+def extract_links(content: str):
+    """only extracts media links based on markdown `![$name]($link)` format"""
+    files = list()
+
+    if (
+        lnk_matches := re.findall(r"\!\[.+\]\(.*\)", content
+    )) == []:
+        return []
+
+    for embed_txt in lnk_matches:
+        [media_filename, rest] = (
+            embed_txt.split('[')[1]
+        ).split(']')
+
+        media_link = rest[1:-1]
+
+        files.append((media_filename, media_link))
+
+    return files
+
+
+def link_extracter(data_dir: str = "issues"):
+    """Assumes data is split by `requester.py`. Also assumes Linear JSON structure from 14/12/2025:
+    ```
+    {
+        "data": {
+            "issues": {
+                "nodes": [
+                    {
+                        ...
+                        "description": str,
+                        ...
+                        "comments": {
+                            "nodes": [
+                                {
+                                    ...
+                                    "body": str,
+                                },
+                            ],
+                        },
+                        ...
+                    },
+                    ...
+                ]},
+            },
+        },
+    }
+    ```"""
+    files = list()
+
+    for filename in glob.glob(f"{data_dir}/ACC-*.json"):
+        with open(os.path.join(os.getcwd(), filename), 'r') as f:
+            file = json.load(f)
+
+            if not file["description"]:
+                print(f"{filename} doesn't have a 'description'")
+            else:
+                if len((media := extract_links(file["description"]))) > 0:
+                    print(f"In {filename}/description: found matches {media}")
+                    files.extend(media)
+
+
+            if not file["comments"]:
+                print(f"Wait, {filename} doesn't even have 'comments'?")
+                continue
+
+            if not file["comments"]["nodes"]:
+                print(f"aww, no comments in file {filename}")
+                continue
+
+            for comment in file["comments"]["nodes"]:
+                content: str = comment["body"]
+
+                if len((media := extract_links(content))) > 0:
+                    print(f"In {filename}/comments: found matches {media}")
+                    files.extend(media)
+
+
+    with open(os.path.join(os.getcwd(), "media_files.txt"), "w+") as output:
+        for (name, link) in files:
+            output.write(f"\"{name}\",{link},\n")
+
+    return files
+
+
+def get_media(name_link_pairs, target_dir: str = "images"):
+    """retrieves the extracted media files from Linear's servers"""
+    # saving to an `images/`, as that makes sense
+    # if you want to save somewhere else, change `image.save(...)` below, too
+    os.makedirs(target_dir, exist_ok=True)
+
+    headers = {
+        # "Content-Type": "application/json",
+        "Authorization": os.environ.get("LINEAR_API_KEY")
+    }
+
+    for (name, url) in name_link_pairs:
+        if url.find("uploads.linear.app") < 0:
+            print(
+                f"File: '{name}', at url: {url}\n",
+                "does not appear to be a Linear-stored file",
+                "please retrieve it manually, or modify this script"
+            )
+            continue  # is not a linear file to be pulled
+
+        response = requests.get(url=url, headers=headers)
+
+        if response.status_code == 200:
+            image = Image.open(io.BytesIO(response.content))
+            image.save(os.path.join(os.getcwd(), f"{target_dir}/{name}"))
+        else:
+            print(
+                "Encountered error making GraphQL request:\n",
+                f"Status Code: {response.status_code},\n"
+                f"API Key: {os.environ.get("LINEAR_API_KEY")}\n"
+            )
+
+
+if __name__ == "__main__":
+    link_name_pairs = link_extracter("issues")
+
+    get_media(link_name_pairs, "images")
---
+++ requester.py
@@ -0,0 +1,121 @@
+import json
+import os
+import requests
+
+
+# define query up here, to avoid bloating fn definition.
+# feel free to modify the `last: ???` value, I just picked 100
+# because it seemed like enough for my workspace
+request ="""
+query($before: String, $number: Float) {
+    issues(last: 100, before: $before, filter: {number: {eq: $number}}, includeArchived: true) {
+        nodes {
+            id
+            url
+            identifier
+            title
+            description
+            creator {
+                name
+                email
+            }
+            assignee {
+                name
+                email
+            }
+            priorityLabel
+            state {
+                name
+            }
+            project {
+                name
+                description
+            }
+            createdAt
+            labels(last: 20) {
+                nodes {
+                    name
+                    color
+                    description
+                }
+            }
+            comments(last: 10) {
+                nodes {
+                    url
+                    user {
+                        name
+                        email
+                    }
+                    createdAt
+                    body
+                }
+            }
+            attachments(last: 10) {
+                nodes {
+                    url
+                }
+            }
+            relations(last: 10) {
+                nodes {
+                    relatedIssue {
+                        identifier
+                    }
+                }
+            }
+            parent {
+                identifier
+            }
+            children(last: 10) {
+                nodes {
+                    identifier
+                }
+            }
+        }
+    }
+}
+"""
+
+
+def get_data(outputfile: str, url: str = "https://api.linear.app/graphql"):
+    if os.path.isfile(outputfile):
+        print(f"{outputfile} already exists, using cached file")
+        return
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": os.environ.get("LINEAR_API_KEY")
+    }
+
+    response = requests.post(url=url, headers=headers, json={"query": request})
+
+    if response.status_code == 200:
+        js_file = json.loads(response.content.decode('utf-8'))
+        with open(os.path.join(os.getcwd(), "filedump.json"), "w+") as f:
+            json.dump(js_file, f, indent=4)
+    else:
+        raise Exception(
+            "Encountered error making GraphQL request:\n",
+            f"Status Code: {response.status_code},\n"
+            f"API Key: {os.environ.get("LINEAR_API_KEY")}\n"
+        )
+
+
+def split_data(datafile: str, target_dir: str):
+    with open(os.path.join(os.getcwd(), datafile), "r+") as f:
+        data = json.load(f)
+        for node in data["data"]["issues"]["nodes"]:
+            name = node["identifier"]
+            os.makedirs(target_dir, exist_ok=True)
+            with open(
+                os.path.join(os.getcwd(), f"{target_dir}/{name}.json"), "w+"
+            ) as out:
+                json.dump(node, out, indent=4)
+
+
+if __name__ == "__main__":
+    # save as file, to avoid making repeated requests, on processing
+    intermediate_file = "filedump.json"
+
+    get_data(intermediate_file)
+
+    split_data(intermediate_file, "issues")
